6ec08f91bf2db07225ae884314d3fae0fc9de809
shahnewaz-labib <labib.bzs@gmail.com>
2020-12-25 00:44:32 +0600

    changed menu sound

--- main.cpp
+++ main.cpp
@@ -12,69 +12,7 @@
 
 using namespace sf;
 
-//Connector grid[MAX][MAX];
 int n;
-// Connector& cell(sf::Vector2i v) {
-//     return grid[v.x][v.y];
-// }
-
-// bool isOut(sf::Vector2i v, int N) {
-//     return !sf::IntRect(0, 0, N, N).contains(v);
-// }
-
-// void generate(int N) {
-//     std::vector<sf::Vector2i> nodes;
-//     nodes.push_back(sf::Vector2i(rand()%N, rand()%N));
-
-//     while(!nodes.empty()) {
-//         int n = rand()%nodes.size();
-//         sf::Vector2i v = nodes[n];
-//         sf::Vector2i d = DIR[rand()%4];
-
-//         if(cell(v).dirs.size() == 3) {
-//             nodes.erase(nodes.begin() + n);
-//             continue;
-//         }
-
-//         if(cell(v).dirs.size() == 2) {
-//             if(rand()%50) continue;
-//         }
-
-//         bool complete = true;
-//         for(int i = 0; i < 4; i++) {
-//             sf::Vector2i D = DIR[i];
-//             if(!isOut(v + D, N) and cell(v + D).dirs.empty()) {
-//                 complete = false;
-//                 break;
-//             }
-//         }
-
-//         if(complete) {
-//             nodes.erase(nodes.begin() + n);
-//             continue;
-//         }
-
-//         if(isOut(v + d, N)) continue;
-
-//         if(!cell(v + d).dirs.empty()) continue;
-
-//         cell(v).dirs.push_back(d);
-//         cell(v+d).dirs.push_back(-d);
-
-//         nodes.push_back(v+d);
-//     }
-
-// }
-
-// void show(int n) {
-//     for(int i = 0; i < n; i++) {
-//         for(int j = 0; j < n; j++) {
-//             std::cout << grid[i][j].dirs.size() << " ";
-//         }
-//         std::cout << "\n";
-//     }
-// }
-
 
 int main() {
     oikhane:
@@ -92,7 +30,7 @@ int main() {
     clickSound.setBuffer(clickSoundBuffer);
 
     SoundBuffer menuOptionsSoundBuffer;
-    menuOptionsSoundBuffer.loadFromFile("sounds/roblox-death-sound_1.ogg");
+    menuOptionsSoundBuffer.loadFromFile("sounds/menuClick.wav");
     Sound menuOptionsSound;
     menuOptionsSound.setBuffer(menuOptionsSoundBuffer);
 
@@ -201,8 +139,9 @@ int main() {
     while(gameWindow.isOpen()) {
         Event event;
         while(gameWindow.pollEvent(event)) {
-            if(event.type == Event::Closed)
+            if(event.type == Event::Closed or Keyboard::isKeyPressed(Keyboard::Q)) {
                 gameWindow.close();
+            }
             if(Keyboard::isKeyPressed(Keyboard::Escape)) {
                 MENU_STATE = true;
                 GAME_STATE = false;
