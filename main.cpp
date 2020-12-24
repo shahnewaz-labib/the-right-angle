5b62ea09ff1b32f4431382923f2b5d1a90efe7ba
shahnewaz-labib <labib.bzs@gmail.com>
2020-12-25 00:16:03 +0600

    .

--- main.cpp
+++ main.cpp
@@ -12,14 +12,78 @@
 
 using namespace sf;
 
+//Connector grid[MAX][MAX];
+int n;
+// Connector& cell(sf::Vector2i v) {
+//     return grid[v.x][v.y];
+// }
+
+// bool isOut(sf::Vector2i v, int N) {
+//     return !sf::IntRect(0, 0, N, N).contains(v);
+// }
+
+// void generate(int N) {
+//     std::vector<sf::Vector2i> nodes;
+//     nodes.push_back(sf::Vector2i(rand()%N, rand()%N));
+
+//     while(!nodes.empty()) {
+//         int n = rand()%nodes.size();
+//         sf::Vector2i v = nodes[n];
+//         sf::Vector2i d = DIR[rand()%4];
+
+//         if(cell(v).dirs.size() == 3) {
+//             nodes.erase(nodes.begin() + n);
+//             continue;
+//         }
+
+//         if(cell(v).dirs.size() == 2) {
+//             if(rand()%50) continue;
+//         }
+
+//         bool complete = true;
+//         for(int i = 0; i < 4; i++) {
+//             sf::Vector2i D = DIR[i];
+//             if(!isOut(v + D, N) and cell(v + D).dirs.empty()) {
+//                 complete = false;
+//                 break;
+//             }
+//         }
+
+//         if(complete) {
+//             nodes.erase(nodes.begin() + n);
+//             continue;
+//         }
+
+//         if(isOut(v + d, N)) continue;
+
+//         if(!cell(v + d).dirs.empty()) continue;
+
+//         cell(v).dirs.push_back(d);
+//         cell(v+d).dirs.push_back(-d);
+
+//         nodes.push_back(v+d);
+//     }
+
+// }
+
+// void show(int n) {
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < n; j++) {
+//             std::cout << grid[i][j].dirs.size() << " ";
+//         }
+//         std::cout << "\n";
+//     }
+// }
+
+
 int main() {
     oikhane:
     // Main game window
-    RenderWindow gameWindow(VideoMode(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT), "The Right Angle!");
-    gameWindow.setKeyRepeatEnabled(false);
+    RenderWindow menuWindow(VideoMode(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT), "The Right Angle!");
+    menuWindow.setKeyRepeatEnabled(false);
 
     // Menu window
-    Menu menuWindow(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT);
+    Menu menu(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT);
 
     // Sound
     SoundBuffer clickSoundBuffer;
@@ -46,39 +110,39 @@ int main() {
     int n = rand()%10+2;
     generate(n);
 
-    while(gameWindow.isOpen()) {
+    while(menuWindow.isOpen()) {
         // To capture keypresses
         Event event;
         
         // Handle KB or Mouse actions
-        while(gameWindow.pollEvent(event)) {
+        while(menuWindow.pollEvent(event)) {
 
             if(event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Key::Q)) {
-                gameWindow.close();
+                menuWindow.close();
                 exit(0);
             }
 
             if(MENU_STATE) {
                 if(event.type == Event::KeyReleased) {
                     if(event.key.code == Keyboard::Return) {
-                        switch(menuWindow.selectedOptionIndex) {
+                        switch(menu.selectedOptionIndex) {
                             case OPTION_INDEX::Play:
                                 // Play button pressed, enter game state
                                 MENU_STATE = false;
                                 GAME_STATE = true;
-                                gameWindow.close();
+                                menuWindow.close();
                                 goto sheshe;
                                 break;
                             case OPTION_INDEX::Change_Level:
                                 // Added level cycling
-                                menuWindow.levelup();
+                                menu.levelup();
                                 break;
                             case OPTION_INDEX::Options:
                                 // Do something cool
                                 break;
                             case OPTION_INDEX::Exit:
                                 // Exit button pressed, close game
-                                gameWindow.close();
+                                menuWindow.close();
                                 exit(0);
                                 break;
                             default:
@@ -88,11 +152,11 @@ int main() {
                 }
                 if(Keyboard::isKeyPressed(Keyboard::Up)) {
                     menuOptionsSound.play();
-                    menuWindow.goUp();
+                    menu.goUp();
                 }
                 if(Keyboard::isKeyPressed(Keyboard::Down)) {
                     menuOptionsSound.play();
-                    menuWindow.goDown();
+                    menu.goDown();
                 }
                 
 
@@ -111,33 +175,38 @@ int main() {
             
         }
         if(MENU_STATE) {
-            gameWindow.clear(Color::Black);
+            menuWindow.clear(Color::Black);
         }
         else if(GAME_STATE) {
-            gameWindow.clear(Color::Cyan);
+            menuWindow.clear(Color::Cyan);
             // draw background
-            gameWindow.draw(bgSprite);
+            menuWindow.draw(bgSprite);
         }
         if(MENU_STATE) {
-            menuWindow.show(gameWindow);
+            menu.show(menuWindow);
         }
 
-        gameWindow.display();
+        menuWindow.display();
 
     }
 
     sheshe:
-    RenderWindow w1(VideoMode(65*n, 65*n), "Another window");
-    w1.setKeyRepeatEnabled(false);
-    while(w1.isOpen()) {
+    RenderWindow gameWindow(VideoMode(65*n, 65*n), "The Right Angle!");
+    n = rand()%10+4;
+    generate(n);
+    
+    show(n);
+
+    gameWindow.setKeyRepeatEnabled(false);
+    while(gameWindow.isOpen()) {
         Event event;
-        while(w1.pollEvent(event)) {
+        while(gameWindow.pollEvent(event)) {
             if(event.type == Event::Closed)
-                w1.close();
+                gameWindow.close();
             if(Keyboard::isKeyPressed(Keyboard::Escape)) {
                 MENU_STATE = true;
                 GAME_STATE = false;
-                w1.close();
+                gameWindow.close();
                 goto oikhane;
             }
             if(event.type == Event::MouseButtonPressed) {
@@ -147,9 +216,9 @@ int main() {
             }
                 
         }
-        // w1.clear(Color::Green);
-        w1.draw(bgSprite);
-        w1.display();
+        // gameWindow.clear(Color::Green);
+        gameWindow.draw(bgSprite);
+        gameWindow.display();
     }
 
     return 0;
